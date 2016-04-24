f4f9867870eaf51e356ff019317275a9d842f3ed
Eva Zhang <evazhang@berkeley.edu>
2016-04-24 14:58:15 -0700

    added view controllers

---
+++ Homies/Homies/AddTaskViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AddTaskViewController.swift
+//  Homies
+//
+//  Created by Eva on 4/24/16.
+//  Copyright © 2016 samkhano. All rights reserved.
+//
+
+import UIKit
+
+class AddTaskViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
