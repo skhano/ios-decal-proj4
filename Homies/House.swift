845bd6f1f20d7a4addc9080ab5391f117d2de84d
Sam Khano <s.khano@yahoo.com>
2016-04-24 16:41:00 -0700

    fixed the merge conflicts

--- Homies/House.swift
+++ Homies/House.swift
@@ -1,9 +1,9 @@
 //
-//  House.swift
-//  Homies
+//  House.swift
+//  Homies
 //
-//  Created by Sam Khano on 4/18/16.
-//  Copyright © 2016 samkhano. All rights reserved.
+//  Created by Sam Khano on 4/18/16.
+//  Copyright © 2016 samkhano. All rights reserved.
 //
 
 import UIKit
@@ -15,7 +15,7 @@ class House: NSObject {
     var neededItems : [String]!
     var createdDate : NSDate!
     var tasks :[Task]!
-    
+
     init(eHouseName: String, eCreatedDate: NSDate) {
         houseName = eHouseName
         createdDate = eCreatedDate
@@ -27,5 +27,24 @@ class House: NSObject {
             exec = houseUser
         }
     }
-    
+
+    func getLoserName() -> String {
+        sortUsersScore()
+        return users[users.count-1].userName
+    }
+
+    func sortUsersScore() {
+        users.sortInPlace({$0.score > $1.score})
+    }
+
+    func getPurchasedItems() -> NSMutableDictionary {
+        let purchasedItems = NSMutableDictionary(dictionary: [String : Double] ());
+        for user in users {
+            purchasedItems.addEntriesFromDictionary(user.itemsBought)
+        }
+        return purchasedItems
+    }
+    func getNeededItems() -> [String] {
+        return neededItems
+    }
 }
